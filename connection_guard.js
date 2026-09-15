050cb347b9a34fb5f07d49988f6ecb76e86fcb00
amalswarnakar7980-beep <amalswarnakar7980@gmail.com>
2026-09-15 13:04:07 +0530

    Add ConnectionGuard for network status monitoring
    Implement ConnectionGuard to manage online/offline status.

--- connection_guard.js
+++ connection_guard.js
@@ -1,1 +1,19 @@
+// connection_guard.js - Safe Network Recovery
+
+const ConnectionGuard = {
+  isOnline: true,
+
+  init() {
+    window.addEventListener("online", () => {
+      this.isOnline = true;
+      window.dispatchEvent(new CustomEvent("connection_restored"));
+    });
+
+    window.addEventListener("offline", () => {
+      this.isOnline = false;
+    });
+  }
+};
+
+ConnectionGuard.init();
 
